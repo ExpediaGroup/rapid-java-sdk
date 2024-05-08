a8666a95b228e58b8ec6ca9b4e68a6e1647efbff
Dana AlTayeh <daltayeh@expediagroup.com>
2024-05-08 11:53:20 +0300

    SDK-1182 add a new scenarios that presents how to book multiple rooms with hold and resume booking

---
+++ examples/src/main/java/com/expediagroup/sdk/rapid/examples/scenarios/booking/MultiRoomHoldAndResumeBookScenario.java
@@ -0,0 +1,88 @@
+package com.expediagroup.sdk.rapid.examples.scenarios.booking;
+
+import com.expediagroup.sdk.core.model.Nothing;
+import com.expediagroup.sdk.core.model.Response;
+import com.expediagroup.sdk.rapid.examples.Constants;
+import com.expediagroup.sdk.rapid.examples.salesprofiles.RapidPartnerSalesProfile;
+import com.expediagroup.sdk.rapid.examples.scenarios.RapidScenario;
+import com.expediagroup.sdk.rapid.examples.services.BookService;
+import com.expediagroup.sdk.rapid.examples.services.ShopService;
+import com.expediagroup.sdk.rapid.models.Itinerary;
+import com.expediagroup.sdk.rapid.models.ItineraryCreation;
+import com.expediagroup.sdk.rapid.models.Property;
+import com.expediagroup.sdk.rapid.models.PropertyAvailability;
+import com.expediagroup.sdk.rapid.models.RoomPriceCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiRoomHoldAndResumeBookScenario implements RapidScenario {
+
+    private static final Logger logger = LoggerFactory.getLogger(MultiRoomHoldAndResumeBookScenario.class);
+    private RapidPartnerSalesProfile rapidPartnerSalesProfile;
+
+    @Override
+    public void setProfile(RapidPartnerSalesProfile rapidPartnerSalesProfile) {
+        this.rapidPartnerSalesProfile = rapidPartnerSalesProfile;
+    }
+
+    @Override
+    public void run() {
+
+        // Shopping for properties
+        /*
+         * To request multiple rooms (of the same type), include one instance of occupancy for each room requested.
+         * Up to 8 rooms may be requested or booked at once.
+         */
+        logger.info("Getting property availability for test property: [{}]", Constants.TEST_PROPERTY_ID);
+        List<String> occupancy = Arrays.asList("2", "2");
+        ShopService shopService = new ShopService();
+        List<Property> propertyAvailabilityList = shopService.getMultipleRoomsPropertiesAvailability(this.rapidPartnerSalesProfile, occupancy);
+
+        if (propertyAvailabilityList == null || propertyAvailabilityList.isEmpty()) {
+            logger.error("No property availability found for the test property.");
+            return;
+        }
+
+        logger.info("Property Availability found for property id: [{}] with status: [{}]",
+                propertyAvailabilityList.get(0).getPropertyId(),
+                propertyAvailabilityList.get(0).getStatus());
+
+        // Check room prices for the property
+        logger.info("Checking room prices for property id: [{}]...", Constants.TEST_PROPERTY_ID);
+        PropertyAvailability propertyAvailability = (PropertyAvailability) propertyAvailabilityList.get(0);
+        RoomPriceCheck roomPriceCheck = null;
+
+        roomPriceCheck = shopService.checkRoomPrices(propertyAvailability);
+        logger.info("Room price check status: [{}]", roomPriceCheck.getStatus());
+
+        /*
+         * Book multiple rooms in the property.
+         * The Booking link from your previous Price Check response expires after a short period.
+         * If you receive an HTTP 503 error upon your first attempt, the link has likely expired.
+         * Obtain a new link and attempt your booking again.
+         */
+        logger.info("Booking 2 rooms with [hold=true] in test property: [{}]...", Constants.TEST_PROPERTY_ID);
+        BookService bookService = new BookService();
+        ItineraryCreation itineraryCreation = bookService.createBookingWithHold(roomPriceCheck, occupancy);
+        logger.info("Booking with hold Success. Itinerary id: [{}]. Link to resume booking: [{}]",
+                itineraryCreation.getItineraryId(), itineraryCreation.getLinks().getResume().getHref());
+
+        // Resume the booking process
+        logger.info("Resuming the booking process...");
+        Response<Nothing> response = bookService.resumeBooking(itineraryCreation);
+        logger.info("Resume booking response status: [{}]", response.getStatusCode());
+
+        // Make a retrieve call to verify the booking has been resumed properly.
+        logger.info("Getting itinerary by itinerary id: [{}] to verify the booking has been resumed successfully...",
+                itineraryCreation.getItineraryId());
+        Itinerary itinerary = bookService.getReservationByItineraryId(itineraryCreation);
+
+        logger.info("Itinerary rooms status after resume booking:");
+        itinerary.getRooms().forEach(room ->
+            logger.info("Room: [{}], Status: [{}]", room.getId(), room.getStatus())
+        );
+    }
+}
--- examples/src/main/java/com/expediagroup/sdk/rapid/examples/services/BookService.java
+++ examples/src/main/java/com/expediagroup/sdk/rapid/examples/services/BookService.java
@@ -1,5 +1,7 @@
 package com.expediagroup.sdk.rapid.examples.services;
 
+import com.expediagroup.sdk.core.model.Nothing;
+import com.expediagroup.sdk.core.model.Response;
 import com.expediagroup.sdk.rapid.examples.Constants;
 import com.expediagroup.sdk.rapid.models.BillingContactRequest;
 import com.expediagroup.sdk.rapid.models.BillingContactRequestAddress;
@@ -12,32 +14,100 @@ import com.expediagroup.sdk.rapid.models.PhoneRequest;
 import com.expediagroup.sdk.rapid.models.RoomPriceCheck;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 public class BookService extends RapidService {
 
-    public ItineraryCreation createBooking(RoomPriceCheck roomPriceCheck) {
+    public ItineraryCreation createBooking(RoomPriceCheck roomPriceCheck, List<String> occupancy) {
 
         String bookHref = roomPriceCheck.getLinks().getBook().getHref();
 
-        PhoneRequest phone =
-                PhoneRequest.builder()
-                        .countryCode("1")
-                        .areaCode("487")
-                        .number("5550077")
-                        .build();
+        // In the Book request, include corresponding separate instances of room in the rooms array for each room you wish to book. */
+        List<CreateItineraryRequestRoom> rooms = Collections.nCopies(occupancy.size(), createRoom());
 
-        List<CreateItineraryRequestRoom> rooms = Arrays.asList(
-                CreateItineraryRequestRoom.builder()
-                        .givenName("John")
-                        .familyName("Smith")
-                        .smoking(false)
-                        .specialRequest("Top floor or away from street please")
-                        .build()
+        return rapidClient.postItinerary(
+                Constants.CUSTOMER_IP,
+                Objects.requireNonNull(rapidClient.helpers.extractToken(bookHref)),
+                CreateItineraryRequest.builder()
+                        .affiliateReferenceId(UUID.randomUUID().toString().substring(0, 28))
+                        .hold(false)
+                        .email("john@example.com")
+                        .phone(phone())
+                        .rooms(rooms)
+                        .payments(payments())
+                        .affiliateMetadata("data_point_1:123|data_point2:This is data.")
+                        .taxRegistrationNumber("12345678910")
+                        .travelerHandlingInstructions("Please use the card provided for payment. Avoid cancellation as this is for a corporate traveler. Contact traveler if any issues.")
+                        .build());
+    }
+
+    public ItineraryCreation createBookingWithHold(RoomPriceCheck roomPriceCheck, List<String> occupancy) {
+        String bookHref = roomPriceCheck.getLinks().getBook().getHref();
+
+        // In the Book request, include corresponding separate instances of room in the rooms array for each room you wish to book. */
+        List<CreateItineraryRequestRoom> rooms = Collections.nCopies(occupancy.size(), createRoom());
+
+        // Create a booking with hold, set hold to true
+        return rapidClient.postItinerary(
+                Constants.CUSTOMER_IP,
+                Objects.requireNonNull(rapidClient.helpers.extractToken(bookHref)),
+                CreateItineraryRequest.builder()
+                        .affiliateReferenceId(UUID.randomUUID().toString().substring(0, 28))
+                        .hold(true)
+                        .email("john@example.com")
+                        .phone(phone())
+                        .rooms(rooms)
+                        .payments(payments())
+                        .affiliateMetadata("data_point_1:123|data_point2:This is data.")
+                        .taxRegistrationNumber("12345678910")
+                        .travelerHandlingInstructions("Please use the card provided for payment. Avoid cancellation as this is for a corporate traveler. Contact traveler if any issues.")
+                        .build());
+    }
+
+    public Response<Nothing> resumeBooking(ItineraryCreation itineraryCreation) {
+         return rapidClient.putResumeBookingWithResponse(
+                 Constants.CUSTOMER_IP,
+                 itineraryCreation.getItineraryId(),
+                 rapidClient.helpers.extractToken(itineraryCreation.getLinks().getResume().getHref()),
+                "123455656565",
+                "standard"
+         );
+    }
+
+    public Itinerary getReservationByItineraryId(ItineraryCreation itineraryCreation) {
+        return rapidClient.getReservationByItineraryId(
+                Constants.CUSTOMER_IP,
+                itineraryCreation.getItineraryId(),
+                "123455656565",
+                "standard",
+                rapidClient.helpers.extractToken(itineraryCreation.getLinks().getRetrieve().getHref())
         );
+    }
 
+    /* Helper methods */
+    private CreateItineraryRequestRoom createRoom() {
+        return CreateItineraryRequestRoom.builder()
+                .givenName("John")
+                .familyName("Smith")
+                .smoking(false)
+                .specialRequest("Please provide a room with a view of the ocean.")
+                .build();
+    }
+
+    /* Helper methods */
+    private PhoneRequest phone() {
+        return PhoneRequest.builder()
+                .countryCode("1")
+                .areaCode("487")
+                .number("5550077")
+                .build();
+    }
+
+    /* Helper methods */
+    private List<PaymentRequest> payments() {
         BillingContactRequestAddress address =
                 BillingContactRequestAddress.builder()
                         .line1("555 1st St")
@@ -49,48 +119,20 @@ public class BookService extends RapidService {
                         .postalCode("98121")
                         .build();
 
-        BillingContactRequest billingContact =
-                BillingContactRequest.builder()
-                        .givenName("John")
-                        .familyName("Smith")
-                        .address(address)
-                        .build();
+        BillingContactRequest billingContact = BillingContactRequest.builder()
+                .givenName("John")
+                .familyName("Smith")
+                .address(address)
+                .build();
 
-        List<PaymentRequest> payments = Arrays.asList(
-                PaymentRequest.builder()
-                        .type(PaymentRequest.Type.CUSTOMER_CARD)
-                        .number("4111111111111111")
-                        .securityCode("123")
-                        .expirationMonth("08")
-                        .expirationYear("2025")
-                        .billingContact(billingContact)
-                        .enrollmentDate("2018-09-15")
-                        .build()
-        );
-
-        return rapidClient.postItinerary(
-                Constants.CUSTOMER_IP,
-                Objects.requireNonNull(rapidClient.helpers.extractToken(bookHref)),
-                CreateItineraryRequest.builder()
-                        .affiliateReferenceId(UUID.randomUUID().toString().substring(0, 28))
-                        .hold(false)
-                        .email("john@example.com")
-                        .phone(phone)
-                        .rooms(rooms)
-                        .payments(payments)
-                        .affiliateMetadata("data_point_1:123|data_point2:This is data.")
-                        .taxRegistrationNumber("12345678910")
-                        .travelerHandlingInstructions("Please use the card provided for payment. Avoid cancellation as this is for a corporate traveler. Contact traveler if any issues.")
-                        .build());
-    }
-
-    public Itinerary getReservationByItineraryId(ItineraryCreation itineraryCreation) {
-        return rapidClient.getReservationByItineraryId(
-                Constants.CUSTOMER_IP,
-                itineraryCreation.getItineraryId(),
-                "123455656565",
-                "standard",
-                rapidClient.helpers.extractToken(itineraryCreation.getLinks().getRetrieve().getHref())
-        );
+        return Arrays.asList(PaymentRequest.builder()
+                .type(PaymentRequest.Type.CUSTOMER_CARD)
+                .number("4111111111111111")
+                .securityCode("123")
+                .expirationMonth("08")
+                .expirationYear("2025")
+                .billingContact(billingContact)
+                .enrollmentDate("2018-09-15")
+                .build());
     }
 }
