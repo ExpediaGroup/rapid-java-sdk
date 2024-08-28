9e8056ab932c47594854ce3f7bb42ce3b4c06e7b
dtayeh <9638552+dtayeh@users.noreply.github.com>
2024-08-28 11:22:35 +0000

    chore: Publish v5.0.0

--- code/src/main/kotlin/com/expediagroup/sdk/rapid/operations/CommitChangeOperationContext.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/rapid/operations/CommitChangeOperationContext.kt
@@ -22,8 +22,10 @@ package com.expediagroup.sdk.rapid.operations
  */
 data class CommitChangeOperationContext(
     val customerIp: kotlin.String,
-    val customerSessionId: kotlin.String? = null,
-    val test: kotlin.String? = null
+    val customerSessionId: kotlin.String? =
+        null,
+    val test: CommitChangeOperationParams.Test? =
+        null
 ) {
     companion object {
         @JvmStatic
@@ -33,7 +35,7 @@ data class CommitChangeOperationContext(
     class Builder(
         private var customerIp: kotlin.String? = null,
         private var customerSessionId: kotlin.String? = null,
-        private var test: kotlin.String? = null
+        private var test: CommitChangeOperationParams.Test? = null
     ) {
         /**
          * @param customerIp IP address of the customer, as captured by your integration. Send IPV4 addresses only.<br> Ensure your integration passes the customer's IP, not your own. This value helps determine their location and assign the correct payment gateway.<br> Also used for fraud recovery and other important analytics.
@@ -48,7 +50,7 @@ data class CommitChangeOperationContext(
         /**
          * @param test The change call has a test header that can be used to return set responses with the following keywords:<br> * `standard` - Requires valid test booking. * `service_unavailable` * `unknown_internal_error`
          */
-        fun test(test: kotlin.String) = apply { this.test = test }
+        fun test(test: CommitChangeOperationParams.Test) = apply { this.test = test }
 
         fun build(): CommitChangeOperationContext {
             validateNullity()
@@ -69,9 +71,15 @@ data class CommitChangeOperationContext(
 
     fun getHeaders(): Map<String, String> {
         return buildMap {
-            customerIp.also { put("Customer-Ip", customerIp) }
-            customerSessionId?.also { put("Customer-Session-Id", customerSessionId) }
-            test?.also { put("Test", test) }
+            customerIp.also {
+                put("Customer-Ip", customerIp)
+            }
+            customerSessionId?.also {
+                put("Customer-Session-Id", customerSessionId)
+            }
+            test?.also {
+                put("Test", test.value)
+            }
         }
     }
 }
