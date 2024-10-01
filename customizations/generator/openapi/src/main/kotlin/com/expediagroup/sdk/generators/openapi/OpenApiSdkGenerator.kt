cbe4c55dc6593773a9220cb60cae8368802b3209
Nasser Anssari <nanssari@expediagroup.com>
2024-10-01 14:42:21 +0300

    fixup

--- customizations/generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/OpenApiSdkGenerator.kt
+++ customizations/generator/openapi/src/main/kotlin/com/expediagroup/sdk/generators/openapi/OpenApiSdkGenerator.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.expediagroup.sdk.generators.openapi
 
 import com.expediagroup.sdk.model.ClientGenerationException
 import com.expediagroup.sdk.product.Product
