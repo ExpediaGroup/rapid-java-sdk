118460a2a6740bc3d1ee14ce5bebf4d04a10ce01
dtayeh <9638552+dtayeh@users.noreply.github.com>
2025-03-26 13:25:17 +0000

    chore: Publish v5.5.0-SNAPSHOT

--- code/src/main/kotlin/com/expediagroup/sdk/rapid/operations/GetCategoriesReferenceOperation.kt
+++ code/src/main/kotlin/com/expediagroup/sdk/rapid/operations/GetCategoriesReferenceOperation.kt
@@ -13,4 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.expediagroup.sdk.rapid.operations
 
+import com.expediagroup.sdk.core.model.Nothing
+import com.expediagroup.sdk.core.model.Operation
+
+/**
+ * Categories Reference
+ * @property params [GetCategoriesReferenceOperationParams]
+ */
+class GetCategoriesReferenceOperation(
+    params: GetCategoriesReferenceOperationParams
+) : Operation<
+        Nothing
+    >(
+        "/v3/references/categories",
+        "GET",
+        "getCategoriesReference",
+        null,
+        params
+    )
